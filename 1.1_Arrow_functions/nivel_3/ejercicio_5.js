69c526d213ad7ddcdfed20901a866402206978e6
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-23 12:55:45 +0100

    Sprint-1.1-ejercicio-5

---
+++ 1.1_Arrow_functions/nivel_3/ejercicio_5.js
@@ -0,0 +1,7 @@
+// Funció de fletxa amb 'setTimeout': Crea una funció de fletxa que imprimeixi un missatge a la consola després d'esperar 3 segons.
+
+let printAfterSomeTime = () => {
+    setTimeout(() => {
+        console.log("Hello, this message will appear after 3 seconds.");
+    }, 3000);
+};
