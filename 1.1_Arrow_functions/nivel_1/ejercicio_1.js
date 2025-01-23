53b19d10657c631ede3a3a96be57dc07edaf3698
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-23 12:27:10 +0100

    Sprint-1.1-ejercicio-1

---
+++ 1.1_Arrow_functions/nivel_1/ejercicio_1.js
@@ -0,0 +1,5 @@
+// Conversió de funcions: Tens una funció add que accepta dos paràmetres i retorna la seva suma. Converteix-la en una funció de fletxa. Per exemple: function add(a, b) {return a + b;}.
+
+let add = (a, b) => {
+    return a + b;
+}
