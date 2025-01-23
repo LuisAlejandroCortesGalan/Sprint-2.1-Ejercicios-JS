89502989e70c0f43570b0bcb122b039a00c112f7
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-23 12:37:38 +0100

    Sprint-1.1-ejercicio-2

---
+++ ejercicio_2.js
@@ -0,0 +1,6 @@
+// Funció de fletxa sense paràmetres: Crea una funció de fletxa anomenada randomNumber que no necessiti paràmetres i que retorni un número aleatori entre 0 i 100.
+
+function randomNumber() {
+  return Math.floor(Math.random() * 101);
+}
+
