91c55095de2dce328d5add2e9966fb57d2291cf2
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-23 12:50:28 +0100

    Sprint-1.1-ejercicio-4

---
+++ 1.1_Arrow_functions/nivel_2/ejercicio_4.js
@@ -0,0 +1,7 @@
+// Funció de fletxa dins d'un loop: Crea una funció anomenada printNumbers que accepti un array de números i utilitzi un loop for per imprimir cada número a la consola utilitzant una funció de fletxa.
+
+let printNumbers = (arrayNumeros) => {
+    arrayNumeros.forEach((numero) => {
+        console.log(numero)
+    })
+}
