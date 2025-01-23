835440c508612711251d9d8e4cfd564368d0f7ed
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-23 13:03:33 +0100

    Sprint-1.2-ejercicio-2

---
+++ 1.2_Operador_terneario/nivel_1/ejercicio_2.js
@@ -0,0 +1,5 @@
+// Ús amb operadors de comparació: Escriu una expressió que utilitzi l'operador ternari per determinar quin dels dos nombres donats (num1 i num2) és més gran. Si num1 és més gran, retorna 'num1 és més gran'. Si no, retorna 'num2 és més gran'.
+
+let comparacion = (num1, num2) => {
+    return num1 > num2 ? 'num1 és més gran' : 'num2 és més gran';
+}
