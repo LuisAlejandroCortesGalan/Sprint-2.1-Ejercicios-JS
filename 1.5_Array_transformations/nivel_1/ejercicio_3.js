52610668897837b054c5d4eb332fd98165e4589a
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-29 11:22:45 +0100

    Sprint-1.5-ejercicio-3

---
+++ 1.5_Array_transformations/nivel_1/ejercicio_3.js
@@ -0,0 +1,9 @@
+// Find: Teniu una array de números [1, 10 , 8, 11]. Utilitza la funció find per a trobar el primer número que és major a 10.
+
+
+let numbers = [1, 10, 8, 11];
+
+let mayorADiez = numbers.find(numbers => numbers > 10)
+
+console.log("Si hay un numero mayor a diez es:",mayorADiez);
+
