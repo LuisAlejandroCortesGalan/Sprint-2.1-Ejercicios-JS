1a4279ca0b65f7acc24af14f266e6b78ba29dcb5
LuisAlejandroCortesGalan <alejandro.cortes.bcn@outlook.com>
2025-01-29 13:53:54 +0100

    Arreglando, mejorando,Sprint-1.7-ejercicio-1

--- 1.7_Promises&Async/nivel_1/ejercicio_1.js
+++ 1.7_Promises&Async/nivel_1/ejercicio_1.js
@@ -1,13 +1,16 @@
 // Creació d'una Promesa: Crea una promesa que es resolgui després de 2 segons i que retorni la cadena de text 'Hola, món'.
 
 
-let miPromesa = new Promise ((resolve, reject) => {
-    setTimeout(() => {
-        resolve("Hola, món");
-    }, 2000) 
-})
+function miPromesa() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hola, món");
+        }, 2000)
+    })
+}
 console.log("Promesa con log", miPromesa);
 
 
-miPromesa.then((mensaje) => {
-console.log("Promesa con .then", mensaje)});
+miPromesa().then((response) => {
+    console.log("Promesa con .then", response)
+});
